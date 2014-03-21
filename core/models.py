9f3df31697e09c85f0973ad1a0c5536e5eea6bc9
James Laverack <james@jameslaverack.com>
2014-03-21 01:27:07 +0000

    Refactor Models

--- core/models.py
+++ core/models.py
@@ -1,13 +1,12 @@
 from google.appengine.ext import db
 
-class BlogPost(db.Model):
+class ContentItem(db.Model):
     author = db.StringProperty(required=True)
-    title = db.StringProperty(required=True)
     content = db.StringProperty(required=True)
-    date = db.DateTimeProperty(auto_now_add=True)
+    date = db.DateTimeProperty(auto_now_add=True)    
 
-class Comment(db.Model):
-    author = db.StringProperty(required=True)
-    content = db.StringProperty(required=True)
-    date = db.DateTimeProperty(auto_now_add=True)
+class BlogPost(ContentItem):
+    title = db.StringProperty(required=True)
+
+class Comment(ContentItem):
     blogpost = db.ReferenceProperty(BlogPost, collection_name = "comments")
