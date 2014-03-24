b5d6d8e2996f97057ccf5f84f66a91497bef3f9a
James Laverack <james@jameslaverack.com>
2014-03-24 02:22:24 +0000

    Updates models
    Adds author name field and changes content field to a TextProperty over a StringProperty.

--- core/models.py
+++ core/models.py
@@ -2,7 +2,8 @@ from google.appengine.ext import db
 
 class ContentItem(db.Model):
     author = db.StringProperty(required=True)
-    content = db.StringProperty(required=True)
+    author_name = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
     date = db.DateTimeProperty(auto_now_add=True)    
 
 class BlogPost(ContentItem):
